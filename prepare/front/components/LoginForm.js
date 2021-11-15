021682c85ac7d27405a2e1e0972ff67bd53bf2c3
seeker1207 <jm3420@naver.com>
2021-11-15 17:28:03 +0900

    styled component, 컴포넌트에 css 적용하기 실습

--- prepare/front/components/LoginForm.js
+++ prepare/front/components/LoginForm.js
@@ -1,6 +1,11 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useMemo} from 'react';
 import {Form, Input, Button} from 'antd';
 import Link from 'next/link';
+import styled from 'styled-components';
+
+const ButtonWrapper = styled.div`
+    margin-top: 10px;
+`
 
 const LoginForm = () => {
     const [id, setId] = useState('');
@@ -11,12 +16,14 @@ const LoginForm = () => {
     const onChangePassword = useCallback((e) => {
         setPassword(e.target.value);
     }, []);
+    // const style = useMemo(() => ({ marginTop: 10}), []);
+
     return (
         <Form>
             <div>
                 <label htmlFor="user-id"> 아이디</label>
                 <br/>
-                <Input name="user-id" value={id} onChange={onChangeId} required/>
+                <Input name="user-id" value={id} onChange={onChangeId} required />
             </div>
             <div>
                 <label htmlFor="user-password">비밀번호</label>
@@ -29,10 +36,10 @@ const LoginForm = () => {
                     required
                 />
             </div>
-            <div>
+            <ButtonWrapper>
                 <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
                 <Link href="/signup"><a><Button>회원가입</Button></a></Link>
-            </div>
+            </ButtonWrapper>
         </Form>
     );
 };
